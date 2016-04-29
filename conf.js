d3f89358a278b6cc0e2595da3fe789ef61781808
Andrei Scripcaru <andrei.scripcaru@pure360.com>
2016-04-29 11:05:26 +0300

    fix travis script

--- conf.js
+++ conf.js
@@ -1,4 +1,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test/**.spec.js']
+  specs: ['test/**.spec.js'],
+  capabilities: {
+    browserName: 'firefox'
+  }
 };
